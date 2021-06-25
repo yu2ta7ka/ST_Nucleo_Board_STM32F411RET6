24f5db90af85c378c9bb1fa98b4b5f19226205dc
yu2ta7ka <yuji.tanaak@gmail.com>
2021-06-26 06:46:31 +0900

    hello world with serial communication

--- examples/serial-tx.rs
+++ examples/serial-tx.rs
@@ -6,21 +6,18 @@ use panic_halt as _;
 use cortex_m_rt::entry;
 use stm32f4xx_hal as hal;
 
-use crate::hal::{
-    gpio::gpioa::*, gpio::*, prelude::*, serial::config::Config, serial::Serial, stm32,
-};
+use crate::hal::{gpio::*, prelude::*, serial::config::Config, serial::Serial, stm32};
 
 use core::fmt::Write; // for pretty formatting of the serial output
 
+// sample "hello world"
 #[entry]
 fn main() -> ! {
     let dp = stm32::Peripherals::take().unwrap();
     let cp = cortex_m::peripheral::Peripherals::take().unwrap();
 
-    // init LED object
+    // init GPIO object
     let gpioa = dp.GPIOA.split();
-    let mut led = Led::new(gpioa.pa5);
-
     // init clock object
     let rcc = dp.RCC.constrain();
     let clocks = rcc.cfgr.use_hse(8.mhz()).freeze();
@@ -39,37 +36,22 @@ fn main() -> ! {
     .unwrap();
     let (mut tx, mut _rx) = serial.split();
 
+    // Use nb crate (https://crates.io/crates/nb)
+    // Minimal and reusable non-blocking I/O layer
+
+    // print "hello world"
+    let text = b"hello world\n";
+    for c in text.iter() {
+        nb::block!(tx.write(*c)).unwrap();
+    }
+    // print "this is UART example!"
+    writeln!(&mut tx, "this is {} example!","UART").unwrap();
+
     let mut value: u8 = 0;
     loop {
         // print some value every 500 ms, value will overflow after 255
         writeln!(tx, "value: {:02}\r", value).unwrap();
         value += 1;
         delay.delay_ms(500_u32);
-        led.toggle();
-    }
-}
-
-// LED driver
-struct Led {
-    pin: PA5<Output<PushPull>>,
-}
-
-impl Led {
-    fn new(pin: PA5<Input<Floating>>) -> Led {
-        Led {
-            pin: pin.into_push_pull_output(),
-        }
-    }
-
-    fn turn_on(&mut self) {
-        self.pin.set_high().unwrap();
-    }
-
-    fn turn_off(&mut self) {
-        self.pin.set_low().unwrap();
-    }
-
-    fn toggle(&mut self) {
-        self.pin.toggle().unwrap();
     }
 }
